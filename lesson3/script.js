9394fb960c3407ae7504425684b0633deb069b1b
annashcherbyna <dn290906gas@gmail.com>
2020-07-10 00:33:04 +0300

    add files

---
+++ lesson3/script.js
@@ -0,0 +1,72 @@
+
+// -- створити об'єкт (не меньше 5ти властивостей) який описує
+// - собаку
+// - людину
+// - автомобіль
+// - квартиру
+// - книгу
+let objDog = {
+    age: 3,
+    name: 'Leo',
+    weight: 10,
+    children:[{name: 'bob'},{name:'charly'}],
+    breed: 'colly'
+};
+
+let objPerson ={
+    name: 'Vasia',
+    age: 30,
+    children:[{name: 'bob'},{name:'charly'}],
+    height: 99,
+    education: 'higher'
+}
+
+// -- Створити масив та вивести його в консоль:
+// - з 5 собак
+// - 3 5 людей
+// - з 5 автомобілів
+
+// -- створити об'єкт (не меньше 5ти властивостей) який описує, одна з властивостей обов'язково повинна бути об'єктом,ще одна - масивом
+// - будинок
+// - водій
+// - іграшку
+// - стіл
+// - сумка
+
+
+
+// Дан массив:
+let users = [
+				{name: 'vasya', age: 31, status: false},
+				{name: 'petya', age: 30, status: true},
+				{name: 'kolya', age: 29, status: true},
+				{name: 'olya', age: 28, status: false},
+				{name: 'max', age: 30, status: true},
+				{name: 'anya', age: 31, status: false},
+				{name: 'oleg', age: 28, status: false},
+				{name: 'andrey', age: 29, status: true},
+				{name: 'masha', age: 30, status: true},
+				{name: 'olya', age: 31, status: false},
+				{name: 'max', age: 31, status: true}
+			];
+// - звернутися в відповідну ячейку масиву і відповідний параметр об'єкта і вивести в консольх наступне
+// - статус Андрія
+// - статус Максима
+// - ім'я передостаннього об'єкту
+// - ім'я третього об'єкта
+// - вік Олега
+// - вік Олі
+// - вік + ім'я 5го об'єкта
+// - вік + сатус Анни
+// Приклад: вивести ім'я 1го об'єкта. Відповідь: console.log (users [0] .name). Будьте уважні! 4й об'єкт має індес 3!
+console.log(users[7].status);
+console.log(users[4].status);
+console.log(users[10].status);
+console.log(users[9].name);
+console.log(users[2].name);
+console.log(users[6].age);
+console.log(users[3].age);
+console.log(users[9].age);
+console.log(users[4].age + ' ' + users[4].name);
+console.log(users[5].age + ' ' + users[5].status);
+
