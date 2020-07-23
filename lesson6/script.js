8d57ef7b8eb88f54adcfacb9049d4ce451f52ef6
annashcherbyna <dn290906gas@gmail.com>
2020-07-23 23:50:32 +0300

    finish 6

--- lesson6/script.js
+++ lesson6/script.js
@@ -75,29 +75,25 @@ users.sort(function(a,b){
 });
 console.log(users);
 
- let newUsers = [];
+let [...newUsers] = users;
 
-//  newUsers.forEach(element => {
-//      element.id = Math.round(Math.random()*100);;
-//  });
+console.log('newUsers');
+console.log(newUsers);
 
-newUsers = users.map(function (val) {
-    // newUsers[i] = val;
-    // newUsers[i].id = Math.round(Math.random()*100);
+newUsers.forEach(val => {
     val.id = Math.round(Math.random()*100);
-    return val;
 });
 
-// users.forEach(function(el,i,users){
-//     newUsers.push(el);
-//     newUsers[i].id = Math.round(Math.random()*100);
-// });
+console.log('newUsers');
+console.log(newUsers);
+
+console.log('users');
+console.log(users);
+
+newUsers.sort((a,b) => a.id > b.id);
 
 console.log(newUsers);
 
-console.log(users);
-
-//newUsers
 
 // -- наисать функцию калькулятора с 2мя числами и колбеком
 let calc = function (a,act,b) {
@@ -120,8 +116,6 @@ console.log(calc3(3,'+',4,'*',2));
 // =============КЛАССНАЯ РАБОТА=================
 // =============================================
 // =============================================
-
-
 
 
 let cars = [ {producer:"subaru",model: "wrx",year: 2010, color:"blue",type: "sedan",engine: "ej204x",volume: 2,power: 400}, 
@@ -300,15 +294,66 @@ logA(arr4);
 // Після того зробити перевірку досвіду ВСІХ наших водіїв. Якщо досвід водія менший за 5 років, але його вік більший за 25, то необідно відправити його на курси підвищення кваліфікації, що збільшить йому досвід на 1 рік.
 // Також спробуйте порахувати суму, яку потрібно потратити для покупки всіх цих авто в циклі
 
+class Car {
+    constructor(model,year,power,price,driver){
+        this.model = model;
+        this.year = year;
+        this.power = power;
+        this.price = price;
+        this.driver = driver;
+    }
+    repairEngine(){
+        this.power += + 10;
+    }
+    changePrice(x){
+        this.price += this.price*x;
+    }
+    changeDriver(name,age,timeempl){
+        this.driver = new Driver(name,age,timeempl);
+    }
+}
 
+class Driver{
+    constructor(name,age,timeempl){
+        this.name = name;
+        this.age = age;
+        this.timeempl = timeempl;
+    }
+    uplevel(){
+        this.timeempl++;
+    }
+}
 
-let car1 = {id: 1, name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}}, 
-{id: 2, name: 'petya', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 1}}, 
-{id: 3, name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}}, 
-{id: 4, name: 'olya', age: 28, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 90}}, 
-{id: 5, name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}}, 
-{id: 6, name: 'anya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 2}}, 
-{id: 7, name: 'oleg', age: 28, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 22}}, 
+let c1 = new Car("legacy",2010,250,800000,new Driver('Petia',40,20));
+let c2 = new Car("e200",2012,300,700000,new Driver('Petia',40,20));
+let c3 = new Car("cx-5",2018,310,900000,new Driver('Kolia',36,12));
+let c4 = new Car("6000",1027,350,1000000,new Driver('Alla',23,4));
+let c5 = new Car("ck-2",2005,130,350000,new Driver('Sergey',30,7));
+let c6 = new Car("jetta",2010,250,650000,new Driver('Dima',25,5));
+let c7 = new Car("q3",2019,290,770000,new Driver('Olia',26,0.5));
+
+c1.repairEngine();
+c1.changeDriver('Ivan',45,25);
+c2.repairEngine();
+c2.changeDriver('Vova',50,22);
+c3.repairEngine();
+c3.changeDriver('Lilia',24,2);
+
+let cars1 = [c1,c2,c3,c4,c5,c6,c7];
+console.log(cars1);
+
+for (let i = 0; i < cars1.length; i+=2) {
+    cars1[i].repairEngine();
+    cars1[i].changePrice(5);
+}
+
+cars1.forEach(val => {
+    if(val.driver.age>25 && val.driver.timeempl<5){
+        val.driver.uplevel();
+    }
+});
+
+console.log(cars1);
 
 // Задача: дан отсортированный по возрастанию массив целых чисел. Необходимо вернуть наименьший и наибольший индекс заданного элемента.
 // Входные данные: arr — массив целых чисел значения которых по модулю не больше 10. Размер массива не более 10 элементов.
@@ -320,3 +365,17 @@ let car1 = {id: 1, name: 'vasya', age: 31, status: false, address: {city: 'Lviv'
 // Answer: MinIndex = 0, MaxIndex = 0.
 // 2. Key = 4
 // Answer: MinIndex = 3, MaxIndex = 6.
+
+Arr = [1, 2, 3, 4, 4, 4, 4, 7, 7, 9, 14];
+
+let minMaxIdx = (x,Arr) => {
+    let min= Arr.indexOf(x);
+    let max = Arr.lastIndexOf(x);
+    return `MinIndex = ${min}, MaxIndex = ${max}`;
+}
+
+console.log(minMaxIdx(1,Arr));
+
+console.log(minMaxIdx(4,Arr));
+
+//navigator.geolocation.getCurrentPosition((geo) => {  console.log(geo.coords.latitude);  console.log(geo.coords.longitude);})
