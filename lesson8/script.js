44c9209a4d5a59017da7799b76d00d74aa2f9ec8
annashcherbyna <dn290906gas@gmail.com>
2020-08-01 15:36:38 +0300

    finash 8 hometask

--- lesson8/script.js
+++ lesson8/script.js
@@ -21,6 +21,38 @@ document.body.appendChild(area);
 // Пользователь вводит какие-то данные и закрывает страницу (не факт, что он заполнил всю форму).
 // Сделайте так, чтобы при следующем заходе на страницу введенные им ранее данные стояли на своих местах.
 // Сделайте ваш скрипт как можно более универсальным.
+let form3 = document.createElement("form");
+let input3 = document.createElement("input");
+let textarea3 = document.createElement("textarea");
+let checkbox3 = document.createElement("input");
+checkbox3.type = "checkbox";
+
+if(localStorage.getItem("input3")){
+    input3.value = localStorage.getItem("input3");
+}
+if(localStorage.getItem("textarea3")){
+    textarea3.value = localStorage.getItem("textarea3");
+}
+if(localStorage.getItem("checkbox3")){
+    localStorage.getItem("checkbox3") === 'false' ? checkbox3.checked = false : checkbox3.checked = true;
+}
+
+input3.addEventListener('input',()=>{
+    localStorage.setItem("input3", input3.value);
+})
+textarea3.addEventListener('input',()=>{
+    localStorage.setItem("textarea3", textarea3.value);
+})
+checkbox3.addEventListener('click',()=>{
+    localStorage.setItem("checkbox3", checkbox3.checked);
+})
+
+form3.appendChild(input3);
+form3.appendChild(textarea3);
+form3.appendChild(checkbox3);
+
+document.body.appendChild(form3);
+
 
 // -Дан текстареа. В него можно ввести данные, нажать кнопку "сохранить" и они "фикисруются" (в хранилище), затем поредактировать их, затем еще поредактировать и возможно еще.....
 // Требование : хранить историю своих изменений (даже после перезагрузки страницы).
@@ -129,19 +161,44 @@ let dayBirthdayLabel = document.createElement("label");
 dayBirthdayLabel.innerText = "Enter your day of Birthday: "
 let dayBirthday = document.createElement("input");
 
+let btnCreateNew = document.createElement("button");
+btnCreateNew.innerText = "CREATE";
+
 let btnSave = document.createElement("button");
 btnSave.innerText = "SAVE";
 
-btnSave.onclick = ()=>{
+let divShow = document.createElement("div");
+
+
+let menuBook = document.createElement("div");
+let btnCreate = document.createElement("button");
+btnCreate.innerText = "Create new contact";
+let btnShow = document.createElement("button");
+btnShow.innerText = "Show all contacts";
+let form = document.createElement("form");
+
+btnCreate.onclick = () => {
+    formCreate();
+}
+
+btnShow.onclick = () => showAll();
+
+document.body.appendChild(menuBook);
+menuBook.appendChild(btnCreate);
+menuBook.appendChild(btnShow);
+document.body.appendChild(divShow);
+document.body.appendChild(form);
+showAll();
+
+btnCreateNew.onclick = ()=>{
     let tempUser = new User(name.value,phone.value,email.value,company.value,department.value,dayBirthday.value);
     console.log(tempUser);
     users.push(tempUser);
     localStorage.setItem("users",JSON.stringify(users));
 }
 
-function formCreate(){
-    let form = document.createElement("form");
-    document.body.appendChild(form);
+function formCreate(i){
+    divShow.innerHTML = '';
     form.appendChild(nameLabel);
     form.appendChild(name);
     form.appendChild(document.createElement("div")); //разделитель
@@ -159,37 +216,52 @@ function formCreate(){
     form.appendChild(document.createElement("div")); //разделитель
     form.appendChild(dayBirthdayLabel);
     form.appendChild(dayBirthday);
-    form.appendChild(btnSave);
+    
+    if (i){
+        name.value = users[i].name;
+        phone.value = users[i].phone;
+        email.value = users[i].email;
+        company.value = users[i].company;
+        department.value = users[i].department;
+        dayBirthday.value = users[i].dayBirthday;
+        form.appendChild(btnSave);
+
+        btnSave.onclick = ()=>{
+            let tempUser = new User(name.value,phone.value,email.value,company.value,department.value,dayBirthday.value);
+            console.log(tempUser);
+            users[i] = tempUser;
+            localStorage.setItem("users",JSON.stringify(users));
+        }
+    } else form.appendChild(btnCreateNew);
 }
 
-function showAll (tag) {
-    users.forEach(el => {
+function showAll () {
+    form.innerHTML = '';
+    divShow.innerHTML = '';
+    for(let i =0 ;i<users.length;i++) {
+        el = users[i];
         let divUser = document.createElement("div");
         divUser.innerText = JSON.stringify(el);
 
         let btnChange = document.createElement("button");
         btnChange.innerText = "Change";
+        let btnDel = document.createElement("button");
+        btnDel.innerText = "Delete";
         divUser.appendChild(btnChange);
-        tag.appendChild(divUser);
+        divUser.appendChild(btnDel);
+        divShow.appendChild(divUser);
 
         btnChange.onclick = () => {
-            formCreate();
+            formCreate(i);
         }
-    });
+
+        btnDel.onclick =()=>{
+            users.splice(i,1);
+            localStorage.setItem("users",JSON.stringify(users));
+            showAll();
+        }
+    };
 }
 
-let menuBook = document.createElement("div");
-let btnCreate = document.createElement("button");
-btnCreate.innerText = "Create new contact";
-let btnShow = document.createElement("button");
-btnShow.innerText = "Show all contacts";
 
-btnCreate.onclick = () => {
-    formCreate();
-}
 
-btnShow.onclick = () => showAll(document.body);
-
-document.body.appendChild(menuBook);
-menuBook.appendChild(btnCreate);
-menuBook.appendChild(btnShow);
